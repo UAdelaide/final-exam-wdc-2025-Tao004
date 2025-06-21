9570abdd48d2730af622b9daa439a5b15d03d179
Tao004 <126471875+Tao004@users.noreply.github.com>
2025-06-21 16:56:59 +0930

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '/public')));
+
+// Routes
+const walkRoutes = require('./routes/walkRoutes');
+const userRoutes = require('./routes/userRoutes');
+
+app.use('/api/walks', walkRoutes);
+app.use('/api/users', userRoutes);
+
+const session = require('express-session');
+
+
+// Export the app instead of listening here
+module.exports = app;
